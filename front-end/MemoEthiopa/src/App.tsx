9594daa7031e3648f9c1b7d47f47bba5afb2246f
kidus surafel <seeh51593@gmail.com>
2025-03-06 02:11:14 +0300

    Initialize front-end structure with React, TypeScript, and Vite; add basic components, configuration files, and update README

---
+++ front-end/MemoEthiopa/src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+import './App.css'
+import CustomEditor from './m'
+
+function App() {
+
+  return (
+    <>
+     <p>Memo Ethiopa</p>
+    </>
+  )
+}
+
+export default App
